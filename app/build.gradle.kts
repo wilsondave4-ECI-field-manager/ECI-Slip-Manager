6356c1bd81f8010312759d9766851c3b73728a75
wilsondave4-ECI-field-manager <wilson.dave4@gmail.com>
2026-08-18 18:49:29 +0200

    Bump ECI Slip Manager to v0.4 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "za.co.eci.slipmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     buildTypes {
